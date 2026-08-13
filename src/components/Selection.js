54fa9b2eb13f506dbe0a66216730d71f2336a830
ARAVINTH0022 <ARAVINTH0022@users.noreply.github.com>
2026-08-13 12:04:10 +0530

    Implement Passing Handler Prop in React solution

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,18 @@
+import React, { useState } from 'react';
 
+const Selection = (props) => {
+  const { applyColor } = props;
+  const [style, updateSelectionStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => applyColor(updateSelectionStyle)}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  );
+};
+
+export default Selection;
